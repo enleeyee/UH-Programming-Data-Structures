10e26af5230cbcd86d281d5621a20f89fc06d0c6
Enlai <enleyee@gmail.com>
2025-03-13 23:35:36 -0500

    mock question 3 PQLL

---
+++ Exam 2/Mock Questions/PrioirityQueueLinkedList.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    int priority;
+    Node* next;
+
+    Node(int d, int p) : data(d), priority(p), next(nullptr) {}
+};
+
+class PriorityQueue {
+private:
+    Node* head;
+
+public:
+    PriorityQueue() : head(nullptr) {}
+
+    // Function to insert a node based on priority
+    void enqueue(int data, int priority) {
+        Node* newNode = new Node(data, priority);
+
+        // Case 1: Empty list or new node has higher priority than the head
+        if (!head || priority < head->priority) {
+            newNode->next = head;
+            head = newNode;
+        } else {
+            // Case 2: Insert in the correct position
+            Node* temp = head;
+            while (temp->next && temp->next->priority <= priority) {
+                temp = temp->next;
+            }
+            newNode->next = temp->next;
+            temp->next = newNode;
+        }
+    }
+
+    void display() {
+        Node* temp = head;
+        while (temp) {
+            cout << "(" << temp->data << ", P" << temp->priority << ") -> ";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+};
+
+int main() {
+    PriorityQueue pq;
+    pq.enqueue(4, 2);
+    pq.enqueue(5, 1);
+    pq.enqueue(6, 3);
+    pq.enqueue(7, 0);
+    
+    cout << "Priority Queue after enqueue operations:" << endl;
+    pq.display();
+
+    return 0;
+}
